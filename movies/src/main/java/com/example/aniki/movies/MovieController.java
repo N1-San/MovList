043e675de4e3563150190c1ae0aecc8b5cfae479
N1-San <utytenrocks@gmail.com>
2024-10-19 07:39:20 +0530

    CORS issue fix

--- movies/src/main/java/com/example/aniki/movies/MovieController.java
+++ movies/src/main/java/com/example/aniki/movies/MovieController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
